66edcca183f9306c52fc6fa4973a538904b9747e
John <12468676+John3Kim@users.noreply.github.com>
2020-02-13 21:13:48 -0500

    Update Minesweeper.py
    Added flag mode.
    Description changes.

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,17 +1,19 @@
 import random
+import sys
 from typing import List
 
 '''
 Minesweeper
 @author: John3Kim
-@desc: This is a text-based version of the popular game
+@desc: This is a console-based version of the popular game
 Minesweeper.
 '''
 
 '''
 TODO:
 - Make a reveal function that shows whether there is a mine
-  or not
+  or not.  we also need to run a flood algorithm which would 
+- Be flexible in terms of the number of mines used in Minesweeper
 '''
 
 class Minesweeper:
@@ -36,6 +38,7 @@ class Minesweeper:
 
         return arr
 
+    #TODO: Deal with the mines
     def __randomize_mines(self) -> List[List[int]]:
         #max_mines = random.randint(0,(self.length-1)*(self.width-1))
         max_mines = 10
@@ -95,25 +98,58 @@ class Minesweeper:
 
         return False
 
+    # TODO fix
     def reveal_tiles(self, i:int, j:int) -> None:
         # implement zero case
         self.board[i][j] = self.mines[i][j]
 
         if self.board[i][j] == 0:
-            # flood the entire board until we see bordered numbers
+            self.circular_check(i,j)
+            # flood the entire board until we see bordered numbers 
+            # Find all zeros from coordinate (i,j) until we see a number other than zero
 
+    # TODO fix
+    # we shall check the surroundings
+    def circular_check(self,i:int,j:int) -> None: 
+        low_bound_i = i - 1 
+        upper_bound_i = i + 1
+        low_bound_j = j - 1 
+        upper_bound_j = j + 1
+
+        while low_bound_i < upper_bound_i: 
+            while low_bound_j < upper_bound_j: 
+
+                if self.validate_inputs(low_bound_i,low_bound_j) or (low_bound_i,low_bound_j) == (i,j):
+                    continue
+
+                if self.mines[low_bound_i][low_bound_j] == 0:
+                    self.board[low_bound_i][low_bound_j] = self.mines[low_bound_i][low_bound_j]
+
+                low_bound_j += 1 
+            
+            low_bound_i += 1
+
+        
     def validate_inputs(self, i:int, j:int) -> bool:
+        '''
+        Check to see if the index i or j are out of bounds
+        '''
         height_bound = i < 0 or i > self.length-1
         width_bound = i < 0 or i > self.width-1
 
         return True if height_bound or width_bound else False
     
     def show_board(self) -> None:
-        for row in self.board:
-            print(row)
+        print("   " + str([i for i in range(len(self.board[0]))]))
 
-    # Debug
-    def show_mines(self) -> None:
+        for i,row in enumerate(self.board):
+            print(str(i) + ": " + str(row))
+
+    def __show_mines(self) -> None:
+        '''
+        Prints the board that shows all the mines. 
+        This is used only for debugging purposes 
+        '''
         for row in self.mines:
             print(row)
         
@@ -121,16 +157,19 @@ class Minesweeper:
 if __name__ == "__main__":
     minesweeper = Minesweeper(10)
     
-
     while True:
 
         minesweeper.show_board()
 
+        mode = input("Enter (F/f) for flag mode or enter (Q/q) to quit. Press Enter or input any other key to reveal tiles.   ").upper()
 
-        mode = input("Enter F for flag mode or press Enter to reveal tiles ").upper()
-
-        if mode != 'F':
-            ode = None
+        if mode == 'Q': 
+            print("Exiting game... Good bye!")
+            sys.exit()
+        elif mode == 'F':
+            print("Flag mode")
+        else: 
+            print("Reveal tiles mode")
         
         try:
             height = int(input("Input height. "))
@@ -144,7 +183,8 @@ if __name__ == "__main__":
             continue
 
         if mode == 'F':
-            minesweeper =
+            minesweeper.add_flag(height,width)
+            continue
         else:
             minesweeper.reveal_tiles(height,width)
             
