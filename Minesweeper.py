40ffe36903a27c1af557d0abfa186dcc19b29c84
John <12468676+John3Kim@users.noreply.github.com>
2020-02-14 23:48:57 -0500

    Major changes! Fully playable game!
    Constructor has changed so that users can input the dimensions and the number of mines in the console.
    Many auxiliary functions such as is_victory and reveal_tiles implemented
    reveal_tiles reveal surrounding tiles if there are zero mines in its vicinity

--- Minesweeper.py
+++ Minesweeper.py
@@ -9,51 +9,98 @@ Minesweeper
 Minesweeper.
 '''
 
-'''
-TODO:
-- Make a reveal function that shows whether there is a mine
-  or not.  we also need to run a flood algorithm which would 
-- Be flexible in terms of the number of mines used in Minesweeper
-'''
-
 class Minesweeper:
 
-    def __init__(self, length:int, width:int):
-        self.length = length
-        self.width = width
-        self.board = self.__init_board()
-        self.mines = self.__randomize_mines()
-
-    def __init__(self, length:int):
-        self.length = length
-        self.width = length
-        self.board = self.__init_board()
-        self.mines = self.__randomize_mines()
+    def __init__(self):
+        self.length = None
+        self.width = None
+        self.num_mines = None
+        self.num_revealed = 0
+        self.board = None
+        self.mines_board = None
 
     def __init_board(self) -> List[List[int]]:
-        arr = [None for i in range(self.length)]
+        '''
+        Builds the board for Minesweeper. 
+        '''
+        game_board = [None for l in range(self.length)]
 
-        for i in range(self.length):
-            arr[i] = ['#']*self.width
+        for l in range(self.length):
+            game_board[l] = ['#']*self.width
 
-        return arr
+        return game_board
+    
+    def board_settings(self): 
+    
+        while True:
+            try:
+                length = int(input("Please set length of the board.  "))
+                width = int(input("Please set width of the board. "))
+                
+                if length < 1 or width < 1: 
+                    print("Please enter positive whole numbers!")
+                
+                break
 
-    #TODO: Deal with the mines
-    def __randomize_mines(self) -> List[List[int]]:
+            except: 
+                print("Please input whole numbers!")
+                continue
+            
+        self.length = length 
+        self.width = width
+        self.board = self.__init_board()
+
+    
+    def mines_settings(self,is_random:bool=False)-> None:
+        
+        size_board = self.length*self.width
+
+        if is_random: 
+            self.num_mines = random.randint(0,(self.length-1)*(self.width-1)) 
+            self.mines_board = self.__randomize_mines(self.num_mines)
+            return 
+        
+        while True: 
+            
+            num_mines_input = input("How many mines do you want? Choose between 1 and {highest}.  Quit with (Q/q).".format(highest = size_board)).upper()
+            
+            if num_mines_input == 'Q': 
+                sys.exit()
+
+            try:
+                num_mines_input = int(num_mines_input)
+            except: 
+                print("Input whole numbers!")
+                continue
+
+            if 1 <= num_mines_input <= size_board: 
+                break 
+            else: 
+                print("Not within bounds! Input again!")
+
+        
+        self.num_mines = num_mines_input
+        self.mines_board = self.__randomize_mines(self.num_mines)
+
+    def __randomize_mines(self,number_mines:int) -> List[List[int]]:
+        '''
+        Adds mines in the board. 
+        '''
+
         #max_mines = random.randint(0,(self.length-1)*(self.width-1))
-        max_mines = 10
+        max_mines = number_mines
 
-        arr = [None for i in range(self.length)]
+        mines_board = [None for i in range(self.length)]
 
         for i in range(self.length):
-            arr[i] = [0]*self.width
+            mines_board[i] = [0]*self.width
 
-        while max_mines >= 0:
+        while max_mines > 0:
             rndm_length = random.randint(0,self.length-1)
             rndm_width = random.randint(0,self.width-1)
 
-            if arr[rndm_length][rndm_width] != "X":
-                arr[rndm_length][rndm_width] = "X"
+            if mines_board[rndm_length][rndm_width] != "X":
+                mines_board[rndm_length][rndm_width] = "X"
                 max_mines -= 1
 
         # Add the numbers in the mine based on vicinity
@@ -62,11 +109,12 @@ class Minesweeper:
         
         for i in range(self.length):
             for j in range(self.width):
-                if arr[i][j] == "X":
+                if mines_board[i][j] == "X":
 
                     if (i < 0 or i > self.length-1) or(j < 0 or j > self.width-1):
                         continue
                     else:
+
                         for i_mines in range(i-1,i+2):
 
                             if i_mines > self.length-1 or i_mines < 0:
@@ -77,10 +125,28 @@ class Minesweeper:
                                 if j_mines > self.width-1 or j_mines < 0:
                                     continue
                                 
-                                if arr[i_mines][j_mines] != "X":
-                                    arr[i_mines][j_mines] += 1
+                                if mines_board[i_mines][j_mines] != "X":
+                                    mines_board[i_mines][j_mines] += 1
 
-        return arr
+        return mines_board
+    
+    def already_revealed(self, i:int, j:int) -> None: 
+        '''
+        Checks to see if a player has already revealed the tile
+        '''
+        return True if self.board[i][j] not in {'#','F'} else False
+
+    def is_victory(self) -> bool: 
+        num_hashes = self.length*self.width
+
+        # Count the number of hashes removed 
+        for i in range(self.length): 
+            for j in range(self.width): 
+                if self.board[i][j] not in {"#","F"}: 
+                    num_hashes -= 1
+                
+
+        return True if (num_hashes == self.num_mines) else False
 
     def add_flag(self, i:int, j:int) -> None:
         '''
@@ -92,45 +158,40 @@ class Minesweeper:
             self.board[i][j] = "#"
 
     def is_mine(self, i:int, j:int) -> bool:
-        
-        if self.mines[i][j] == "X":
-            return True
+        '''
+        Checks if a tile contains a mine.
+        '''
+        return True if self.mines_board[i][j] == "X" else False
 
-        return False
-
-    # TODO fix
     def reveal_tiles(self, i:int, j:int) -> None:
-        # implement zero case
-        self.board[i][j] = self.mines[i][j]
-
-        if self.board[i][j] == 0:
-            self.circular_check(i,j)
-            # flood the entire board until we see bordered numbers 
-            # Find all zeros from coordinate (i,j) until we see a number other than zero
-
-    # TODO fix
-    # we shall check the surroundings
-    def circular_check(self,i:int,j:int) -> None: 
-        low_bound_i = i - 1 
-        upper_bound_i = i + 1
-        low_bound_j = j - 1 
-        upper_bound_j = j + 1
-
-        while low_bound_i < upper_bound_i: 
-            while low_bound_j < upper_bound_j: 
-
-                if self.validate_inputs(low_bound_i,low_bound_j) or (low_bound_i,low_bound_j) == (i,j):
-                    continue
-
-                if self.mines[low_bound_i][low_bound_j] == 0:
-                    self.board[low_bound_i][low_bound_j] = self.mines[low_bound_i][low_bound_j]
-
-                low_bound_j += 1 
-            
-            low_bound_i += 1
-
+        ''' 
+        Reveals board tiles.  
+        '''
+        number_set = {i for i in range(9)}
         
-    def validate_inputs(self, i:int, j:int) -> bool:
+        self.board[i][j] = self.mines_board[i][j]
+
+        # if we spot a zero, run the algorithm on the surrounding tiles
+        # flood the entire board until we see bordered numbers 
+        # Find all zeros from coordinate (i,j) until we see a number other than zero
+        if self.board[i][j] == 0:
+            
+            for i_mines in range(i-1,i+2):
+
+                if i_mines > self.length-1 or i_mines < 0:
+                    continue
+                                
+                for j_mines in range(j-1,j+2):
+
+                    if j_mines > self.width-1 or j_mines < 0:
+                        continue
+                    
+                    if self.board[i_mines][j_mines] in number_set:
+                        self.board[i][j] = self.mines_board[i][j]
+                    else:
+                        self.reveal_tiles(i_mines,j_mines)
+       
+    def input_out_of_bounds(self, i:int, j:int) -> bool:
         '''
         Check to see if the index i or j are out of bounds
         '''
@@ -145,21 +206,28 @@ class Minesweeper:
         for i,row in enumerate(self.board):
             print(str(i) + ": " + str(row))
 
-    def __show_mines(self) -> None:
+    def show_mines(self) -> None:
         '''
         Prints the board that shows all the mines. 
         This is used only for debugging purposes 
         '''
-        for row in self.mines:
+        for row in self.mines_board:
             print(row)
         
         
 if __name__ == "__main__":
-    minesweeper = Minesweeper(10)
-    
+    minesweeper = Minesweeper()
+
+    minesweeper.board_settings()
+    minesweeper.mines_settings()
+
     while True:
 
         minesweeper.show_board()
+
+        if minesweeper.is_victory(): 
+            print("Congrats! You've managed to clear the field!")
+            break
 
         mode = input("Enter (F/f) for flag mode or enter (Q/q) to quit. Press Enter or input any other key to reveal tiles.   ").upper()
 
@@ -178,7 +246,7 @@ if __name__ == "__main__":
             print("Please input valid numbers...")
             continue
 
-        if minesweeper.validate_inputs(height,width):
+        if minesweeper.input_out_of_bounds(height,width):
             print("Numbers out of bounds.")
             continue
 
@@ -186,13 +254,17 @@ if __name__ == "__main__":
             minesweeper.add_flag(height,width)
             continue
         else:
-            minesweeper.reveal_tiles(height,width)
             
+            if minesweeper.already_revealed(height,width): 
+                print("Already chose this plot.")
+                continue
+
+            minesweeper.reveal_tiles(height,width)
+
             if minesweeper.is_mine(height,width):
                 minesweeper.show_board()
                 print("Game over")
                 break
-            
         
         
     
